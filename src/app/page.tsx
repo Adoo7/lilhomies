0e2d1b963304759c08609cd936532c80d3ad6a0d
adoo <adoobinsarhan@gmail.com>
2025-03-05 05:05:51 +0300

    fixed tag phrase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-10 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div>
         <h1 className="text-4xl h-[34px] font-bold text-center p-2">Lil Homies</h1>
-        <p className="text-center h-[34px]">
-          احنه نلعب فوز فن مو حق نستانس
+        <p className="text-center" style={{paddingBlock: '20px' }}>
+          احنه نلعب فور فن مو حق نستانس
         </p>
       </div>
       <div className="flex flex-col items-center space-y-4">
